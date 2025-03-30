0e27e5811fa736cf1ff406da8ed13c0dc09c024d
Anthony Tast <anthonyftast@gmail.com>
2025-03-30 11:27:22 -0400

    start building gencomp

---
+++ src/pages/GenComp.js
@@ -0,0 +1,11 @@
+// // In your App.js or another relevant component
+
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// import GeneratedComponent from './GeneratedComponent'; // Import the generated component
+
+// export default function Gen() {
+//   return (
+//     <GeneratedComponent />
+//   );
+// }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import Login from "./login";
-import Gemchat from "./gemchat";
+import GeneratedComponent from "./GeneratedComponent";
+import App from "./_app";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,7 +18,11 @@ const geistMono = Geist_Mono({
 export default function Home() {
   return (
     <>
+<<<<<<< HEAD
      <Login />
+=======
+    <Gemchat />
+>>>>>>> ccccdb2 (added gemchat)
     </>
   );
 }
