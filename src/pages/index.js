12d13b993c32d88458864e2d29d6af512678c190
Anthony Tast <anthonyftast@gmail.com>
2025-03-30 13:56:46 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import GeneratedComponent from "./GeneratedComponent";
-import App from "./_app";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
